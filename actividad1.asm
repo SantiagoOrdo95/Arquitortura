9352c5e3d2b0ec47c7228321d4065993b6e19f60
Santiago Ordoñez Rojas <santiagoordo95@gmail.com>
2018-10-02 21:25:44 -0500

    Declarando variables

--- actividad1.asm
+++ actividad1.asm
@@ -1,13 +1,19 @@
+.model small
+.stack 64
+.data
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
+numero1 db 0
+numero2 db 0
+suma db 0
+resta db 0
+multiplicacion db 0
+division db 0
 
-org 100h
+msgn1 db 10,13, "Ingrese el primer numero de 1 digito: "'$'
+msgn2 db 10,13, "Ingrese el segundo numero de 1 digito: "'$'
+msgsum db 10,13, "(1) Suma" '$'
+msgres db 10,13, "(2) Resta" '$'
+msgmul db 10,13, "(3) Multiplicacion" '$'
+msgdiv db 10,13, "(4) Division" '$'
 
-; add your code here
-
-ret
-
-
-
-
+.code
